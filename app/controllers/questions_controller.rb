724aefdcc8bca36beed4d3a01eb4201c8e82785d
Benjamin Arnold <vin.drake1310@gmail.com>
2015-10-30 09:36:16 +0100

    Player und Questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,34 @@
+class QuestionsController < ApplicationController
+
+  def new
+    @question= Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to '/questions'
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:question_therm, :correct_answer)
+  end
+
+end
