73cd5bc7871283927ed8b7878381b3e51d70f74c
Benjamin Arnold <vin.drake1310@gmail.com>
2015-10-30 09:51:36 +0100

    It's runnig!

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :players
+  resources :questions
 
   # Example resource route with options:
   #   resources :products do
